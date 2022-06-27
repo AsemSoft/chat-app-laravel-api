15000d091b9f078ae78f8cc1d8e3ffa1bb17b863
Asem Ahmed <softasem@gmail.com>
2022-06-27 09:48:46 +0300

    create a database and three make models

---
+++ chat-app/database/migrations/2022_06_26_145733_create_pictures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('extinstion');
+            $table->string('name');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
